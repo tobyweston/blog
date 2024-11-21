505357cdb73c1704236e702ffdc232c93df6f7c8
Toby Weston <toby.weston@gmail.com>
2024-11-21 18:15:21 +0000

    https://docs.astro.build/en/guides/deploy/github/ to support custom domain name serving from GH (adding back in the public/resources folder)

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://baddotrobot.com',
 	integrations: [mdx(), sitemap()],
 });
