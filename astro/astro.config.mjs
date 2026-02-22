0a0fab70ea2e22978edfb6a69bc0467c359703ba
Toby Weston <toby.weston@gmail.com>
2026-02-22 13:09:15 +0000

    configure tailwind (somewhere along the lines, it got removed). This messes up all the styles so working towards making it look nice again

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://baddotrobot.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), tailwind()],
 });
