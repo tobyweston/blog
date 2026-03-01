df3c0cf1edbe7e77b9a58a39388e33b4cb043863
Toby Weston <toby.weston@gmail.com>
2026-03-01 12:31:36 +0000

    use configured redirect for a 301 (better UX)

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -9,4 +9,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://baddotrobot.com',
 	integrations: [mdx(), sitemap(), tailwind()],
+	redirects: {
+		'/': '/blog',
+	},
 });
